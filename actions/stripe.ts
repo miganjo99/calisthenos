309e0921b694d111a68c16f2f1e6dd5b58abcf7e
Miguel Gandia <jenkins@pipeline.local>
2026-04-07 11:44:13 +0200

    fixing api version stripe

--- actions/stripe.ts
+++ actions/stripe.ts
@@ -6,7 +6,7 @@ import prisma from '@/lib/prisma'
 import { redirect } from 'next/navigation'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2023-10-16', 
+  apiVersion: '2026-02-25.clover', 
 })
 
 export async function createCheckoutSession() {
--- app/api/webhook/stripe/route.ts
+++ app/api/webhook/stripe/route.ts
@@ -4,7 +4,7 @@ import Stripe from "stripe"
 import prisma from "@/lib/prisma"
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2023-10-16",
+  apiVersion: "2026-02-25.clover",
 })
 
 export async function POST(req: Request) {
